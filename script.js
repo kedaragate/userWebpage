4f47f111f2ed0c4076292c8d97af88c49a783506
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-07-13 14:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ modalCloseBtn.textContent="X";
   const userModalSubDiv = document.createElement("div");
   userModalSubDiv.className = "userDetails";
   userModal.appendChild(userModalSubDiv);
-userModal.classList.add="hideModal";
+userModal.classList.remove="hideModal";
 
   const userModalImg = document.createElement("img");
   userModalImg.className = "userImg";
@@ -225,7 +225,7 @@ userModal.classList.add="hideModal";
 };
 
 modalCloseBtn.addEventListener("click", function(){
-userModal.classList.add="hideModal"
+userModal.classList.add("hideModal")
 
 })
 
